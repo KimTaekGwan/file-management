0c93967d2da66afd3fe9108857bd12896b7a28bc
KimTaekGwan <sbe07032@naver.com>
2024-11-07 19:16:28 +0900

    feat: initial front project structure

---
+++ front-end/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	function handleClick() {
+		console.log('Button clicked');
+	}
+</script>
+
+<div>
+	<h1>Welcome to your library project</h1>
+	<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+
+	<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+	<button on:click={handleClick}>Click me</button>
+</div>
+
